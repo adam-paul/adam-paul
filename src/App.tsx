a5395ee6faeba4c01cd868cf4665d9147822ca43
Adam Paul <notadampaul@protonmail.com>
2025-03-08 13:07:29 -0600

    updates for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio-website/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
